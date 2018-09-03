f4c6d9ec613364525accb59c3d1fb57054427ee4
DESKTOP-DI4G0T8\terLOOK <terlook52285@gmail.com>
2018-09-03 15:10:05 +0700

    Initial commit

---
+++ src/main/java/oneDay/springframework/spring5webapp/Spring5WebAppApplication.java
@@ -0,0 +1,12 @@
+package oneDay.springframework.spring5webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WebAppApplication.class, args);
+	}
+}
